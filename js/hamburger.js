10b51efef4ad9e10a7b4aa55acaa44c76edc5d22
lhommenul <antoinejezequel@protonmail.com>
2021-07-24 11:25:18 +0200

    starting to create the modal menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,3 +1,15 @@
+const btn = document.querySelector('#humberger_btn');
 
-
-
+btn.addEventListener('click',()=>{
+    // Check if the btn is in state OPEN or CLOSE
+    // btn.setAttribute('aria-modal',"true") 
+    if (btn.classList.contains("menu_open")) {
+        // Close the menu by removing or adding a class 
+        // change the aria label
+        btn.setAttribute('aria-label',"Fermer")
+    } else {
+        // Open the menu by removing or adding a class 
+        // change aria label 
+        btn.setAttribute('aria-label',"Ouvert")
+    }
+})
