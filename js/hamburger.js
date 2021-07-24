18082da253c75a02faf901f59fcfb0d49ef42c09
lhommenul <antoinejezequel@protonmail.com>
2021-07-24 15:56:02 +0200

    mise a jour du menu hamburger + mise a jour du template her + ajout du script d'import

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,15 +1,24 @@
-const btn = document.querySelector('#humberger_btn');
+const btn = document.querySelector('#hamburger_btn');
+const nav = document.querySelector('.header_nav');
 
 btn.addEventListener('click',()=>{
     // Check if the btn is in state OPEN or CLOSE
     // btn.setAttribute('aria-modal',"true") 
     if (btn.classList.contains("menu_open")) {
+        console.log("close menu");
         // Close the menu by removing or adding a class 
         // change the aria label
         btn.setAttribute('aria-label',"Fermer")
+        btn.classList.remove("menu_open")
+        btn.textContent = "Menu"
+        nav.classList.remove('header_nav_open')
     } else {
+        console.log("open menu");
+        btn.textContent = "Fermer"
         // Open the menu by removing or adding a class 
         // change aria label 
+        btn.classList.add("menu_open")
         btn.setAttribute('aria-label',"Ouvert")
+        nav.classList.add('header_nav_open')
     }
 })
