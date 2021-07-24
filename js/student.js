d1742064c50ff3d3cc2188e0234cf32f8f254dae
lhommenul <antoinejezequel@protonmail.com>
2021-07-24 17:28:03 +0200

    update

--- js/student.js
+++ js/student.js
@@ -1,4 +1,37 @@
-import {Student} from '/js/slider.js'
+class Student{
+    constructor(props){
+        this.data = props;
+    }
+    generateHtml(){
+        // create Elements
+        let card_student = document.createElement("li"),
+            student_img = document.createElement("img"),
+            student_name = document.createElement("p"),
+            student_link = document.createElement("a");
+        // ==== SET CLASS ====
+        (()=>{
+            card_student.className = "card_student"
+                student_img.className = "student_img"
+                student_name.className = "student_name"
+                student_link.className = "student_link"
+        })();
+        // ==== SET DATA ====
+        (()=>{
+            student_img.src= this.data.src;
+            student_img.srcset= this.data.srcset;
+            student_name.innerText = this.data.student_name;
+            student_link.innerText = "En savoir plus";
+            student_link.href = this.data.href;
+        })();
+        // ==== APPEND DATA ====
+        (()=>{
+            card_student.appendChild(student_img)
+            card_student.appendChild(student_name)
+            card_student.appendChild(student_link)
+        })();
+        return card_student;
+    }
+}
 
 new Slider({
     per_pages:16,
